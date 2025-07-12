b65867247a275fd4e436961624cb137776e0990d
winchester-cell <manili.mm91@gmail.com>
2025-07-12 15:46:53 +0330

    map area and markers

--- src/utils/mapUtils/generateMap.js
+++ src/utils/mapUtils/generateMap.js
@@ -9,20 +9,29 @@ const generateMap = (mapRef, theme, darkmap, lightmap) => {
     ).addTo(mapRef.current);
 
 
-    const rashtIndustrialAreaCoords = [
-        [37.24393, 49.56492],
-        [37.24379, 49.52809],
-        [37.22534, 49.52895],
-        [37.22513, 49.56603],
-    ];
+    // const rashtIndustrialAreaCoords = [
+    //     [37.24393, 49.56492],
+    //     [37.24379, 49.52809],
+    //     [37.22534, 49.52895],
+    //     [37.22513, 49.56603],
+    // ];
 
-    L.polygon(rashtIndustrialAreaCoords, {
+    // L.polygon(rashtIndustrialAreaCoords, {
+    //     color: '#86efac',
+    //     weight: 2,
+    //     fillColor: '#86efac',
+    //     fillOpacity: 0.2,
+    // }).addTo(mapRef.current)
+    //     .bindPopup("محدوده شهرک صنعتی رشت");
+
+
+    L.circle([37.23562, 49.54586], {
+        radius: 1500, // شعاع به متر (مثلاً 1000 متر = 1 کیلومتر)
         color: '#86efac',
-        weight: 2,
         fillColor: '#86efac',
         fillOpacity: 0.2,
     }).addTo(mapRef.current)
-        .bindPopup("محدوده شهرک صنعتی رشت");
+        .bindPopup("شعاع تقریبی اطراف شهرک صنعتی رشت");
 
 
 };
