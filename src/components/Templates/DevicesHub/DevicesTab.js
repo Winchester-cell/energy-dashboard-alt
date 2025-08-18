ca514f55475e1ef43b8e3a2898aa6cc8d9163080
winchester-cell <manili.mm91@gmail.com>
2025-08-18 12:33:51 +0330

    data cache and loading fix and responsive grid

--- src/components/Templates/DevicesHub/DevicesTab.js
+++ src/components/Templates/DevicesHub/DevicesTab.js
@@ -17,7 +17,7 @@ export default function DevicesTab() {
                         <div className='w-full h-full flex items-center justify-center'><Loading /></div>
                     }
                     <SimpleBar className='rounded-xl bg-[var(--colBg)] inner-shadow' style={{ height: '100%' }}>
-                        <div className='w-full flex-grow grid grid-cols-5 gap-5 p-3'>
+                        <div className='w-full flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 p-3'>
                             {devicesData?.results?.map(device => {
                                 return <DeviceCard key={device.id} {...device} />
                             })}
